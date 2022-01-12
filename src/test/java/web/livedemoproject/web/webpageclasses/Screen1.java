621722dd5b98237f639b3acf91a94f826e1e994d
santhosh-annam <santhosh.annam@devrabbit.com>
2022-01-12 13:17:23 +0530

    latest execution

---
+++ src/test/java/web/livedemoproject/web/webpageclasses/Screen1.java
@@ -0,0 +1,9 @@
+package web.livedemoproject.web.webpageclasses;
+
+public class Screen1 { 
+
+	public String _adskeprivacypagelogo = "//div[@id='adsk-eprivacy-page-logo']";
+	public String _adskeprivacyconfirmexitlogo = "//div[@id='adsk-eprivacy-confirm-exit-logo']";
+	public String _adskeprivacyconfirmexitdialog = "//div[@id='adsk-eprivacy-confirm-exit-dialog']";
+
+ }
