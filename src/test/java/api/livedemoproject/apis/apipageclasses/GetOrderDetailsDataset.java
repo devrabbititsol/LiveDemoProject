89c2262930599ad353319eec47fab0b928ca890c
santhosh-annam <santhosh.annam@devrabbit.com>
2022-01-12 14:29:59 +0530

    latest execution

---
+++ src/test/java/api/livedemoproject/apis/apipageclasses/GetOrderDetailsDataset.java
@@ -0,0 +1,36 @@
+package api.livedemoproject.apis.apipageclasses; 
+
+public class GetOrderDetailsDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "livedemoproject";
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC_TC1-Default";
+
+public static final String datasetResources1 = "getOrderDetails?user_id=google-oauth2|103072056434732149590&order_id=126";
+public static final int requestType1 = 1;
+public static final int bodyType1 = 0;
+public static final String apiName1 = "Get Order Details";
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+public static final String authenticationData1 = "{\"authtype\":\"bearertoken\","
+ + "\"basicauth\":{\"username\":\"\","
+ + "\"password\":\"devrabbit\"},"
+ + "\"apikey\":{\"key\":\"\","
+ + "\"value\":\"\","
+ + "\"addto\":\"header\"},"
+ + "\"bearertoken\":{\"token\":\"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InBxdVh1YXFScTJQczdqejhoRVNLLSJ9.eyJuaWNrbmFtZSI6InBpY3N0YXIiLCJuYW1lIjoicGljc3RhcmZhbiIsInBpY3R1cmUiOiJodHRwczovL3MzLnVzLXdlc3QtMi5hbWF6b25hd3MuY29tL3BpY3N0YXIvcHJvZmlsZV9waWNzLzM2YzMzM2VjLTMyYzItNDU5OS1iMWFlLWMxMDIwOWE2ZDQwMy5qcGVnIiwidXBkYXRlZF9hdCI6IjIwMjEtMDgtMTdUMDY6MDI6MDAuMjg4WiIsImVtYWlsIjoicGljc3RhckBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzcyI6Imh0dHBzOi8vZGV2LWZ3aHVnbXVzLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2MGUzZjE0YTY0MjQ0MjAwNzExYmY2NjIiLCJhdWQiOiJLNGFRR0lmdG9TdmYxT1pWYXN4OWVtTWprRkY1REN3YSIsImlhdCI6MTYyOTE4MDEyMCwiZXhwIjoxNjM5NTQ4MjUyfQ.AnpXeT7kJ4hxbA6EQC18yb3dUZEDInjzdiSIbdWZRaid9sPcQfmkGBpbq7xfT78oYLESmqEqABYv7FIdytvv9C72G0zNsKro-edAFFPetljNthaq55lEVqQiJmVPQfmgTkYV9WzXgl6-raDx9trOuC43ZTOWO0WKBJc5NPH8pTsa0s1JRdw195VdwSBzpXMsn95eDsFS1Z2SRu_SXx8Sc2355r-y-aZjvXXWoqNJCInylWn0vuypKGvG_FsDb7tSaArT_fNM088S6R6DectPMHMPA93qZ9qLM9N5NDO6V6SmXvkEqd1IzpGFt6Z0KT9YbXx4L-w4XNqPn8Y66EqUTg\"},"
+ + "\"oauth2\":{\"tokenurl\":\"\","
+ + "\"clientid\":\"\","
+ + "\"clientsecret\":\"\"}}";
+
+
+ public static String body1  = "{}";
+
+}
