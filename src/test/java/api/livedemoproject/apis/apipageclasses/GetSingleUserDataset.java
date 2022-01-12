c69b171c2e4333ed0357ab2313c0e6bfef520731
santhosh-annam <santhosh.annam@devrabbit.com>
2022-01-12 16:48:04 +0530

    latest execution

---
+++ src/test/java/api/livedemoproject/apis/apipageclasses/GetSingleUserDataset.java
@@ -0,0 +1,36 @@
+package api.livedemoproject.apis.apipageclasses; 
+
+public class GetSingleUserDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "livedemoproject";
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC_GetAllUers-Default";
+
+public static final String datasetResources1 = "api/users/2";
+public static final int requestType1 = 1;
+public static final int bodyType1 = 0;
+public static final String apiName1 = "Get Single User";
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+public static final String authenticationData1 = "{\"authtype\":\"noauth\","
+ + "\"basicauth\":{\"username\":\"\","
+ + "\"password\":\"devrabbit\"},"
+ + "\"apikey\":{\"key\":\"\","
+ + "\"value\":\"\","
+ + "\"addto\":\"header\"},"
+ + "\"bearertoken\":{\"token\":\"\"},"
+ + "\"oauth2\":{\"tokenurl\":\"\","
+ + "\"clientid\":\"\","
+ + "\"clientsecret\":\"\"}}";
+
+
+ public static String body1  = "{}";
+
+}
---
+++ src/test/java/api/livedemoproject/apis/apitestclasses/TC_GetSingleUser.java
@@ -0,0 +1,41 @@
+package api.livedemoproject.apis.apitestclasses;
+
+import com.restassured.services.*;
+import org.testng.annotations.Test;
+import com.utilities.BaseClass;
+import com.utilities.QFCustomizedCode;
+import com.utilities.ConfigFilesUtility;
+import api.livedemoproject.apis.apipageclasses.GetSingleUserDataset;
+public class TC_GetSingleUser extends BaseClass {
+	private ConfigFilesUtility configFileObj;
+
+	public TC_GetSingleUser() throws Exception {
+		configFileObj= new ConfigFilesUtility();
+		configFileObj.loadPropertyFile("TC_GetSingleUser.properties");
+	}
+
+	@Test(priority = 1)
+	public void doRequest() throws Exception { 
+	
+		for(int i = 1; i <= configFileObj.getIntProperty("datasetsLength"); i++) {
+			boolean isExecutionFlag = true;			
+		/*--------------------- GET --------------------- */
+
+			if(isExecutionFlag) {
+ 			 	Object object = (Object) new GetSingleUserDataset();
+				Class<?> c = object.getClass();
+				String urlParams = (String) c.getField("urlParams" + i).get(object);
+				if(urlParams == null) return;
+				String headers = (String) c.getField("headers" + i).get(object);
+				String body = (String) c.getField("body" + i).get(object);
+				String authenticationData = (String) c.getField("authenticationData" + i).get(object);
+				String apiName = (String) c.getField("apiName" + i).get(object);
+				String datasetResources = (String) c.getField("datasetResources" + i).get(object);
+				int reqType = (Integer) c.getField("requestType" + i).get(object);
+				int bodyType = (Integer) c.getField("bodyType" + i).get(object);
+				String header = (String) c.getField("datasetHeader" + i).get(object);
+				String response = APIService.callRequest(configFileObj,apiName, urlParams, headers, reqType, bodyType, body, configFileObj.getProperty("dataset" +i), datasetResources, authenticationData, formurlEncodedData, formData, linkParams,statusParams);
+				QFCustomizedCode.customCode(apiName, body, response,"livedemoproject");
+			}
+		}
+	}}
