2dff42bd9656c354c38dad31a67450795d097a8e
santhosh-annam <santhosh.annam@devrabbit.com>
2022-02-24 16:09:39 +0530

    latest execution

--- src/test/java/web/livedemoproject/idlyexpressweb/webtestclasses/TC_OrderIdlyTestcase.java
+++ src/test/java/web/livedemoproject/idlyexpressweb/webtestclasses/TC_OrderIdlyTestcase.java
@@ -50,91 +50,6 @@ public class TC_OrderIdlyTestcase extends BaseClass {
 		}
 	}
 
-	private void idlySelectionTest(int i) throws Exception {
-
-		IdlySelection idlySelectionObj = new IdlySelection();
-		try { 
-			windowHandle(driver);
-						qf.wait(driver, idlySelectionObj._close);
-			qf.clickAction(driver, idlySelectionObj._close);
-			printSuccessLogAndReport( "Clicked on Close");
-			windowHandle(driver);
-						windowHandle(driver);
-						windowHandle(driver);
-						qf.clickAction(driver, idlySelectionObj._idlyV, configFileObj.getBooleanProperty("idlyV2"  + i + "click"));
-			if(configFileObj.getBooleanProperty("idlyV2"  + i + "click"))
-			printSuccessLogAndReport( "Clicked on Idly (V)");
-			windowHandle(driver);
-						qf.clickAction(driver, idlySelectionObj._addtoCart, configFileObj.getBooleanProperty("addtoCart2"  + i + "click"));
-			if(configFileObj.getBooleanProperty("addtoCart2"  + i + "click"))
-			printSuccessLogAndReport( "Clicked on Add to Cart");
-
-		} catch (Exception e) {
-			isElementDispalyed = false;
-			printFailureLogAndReport(   "Element is not found" + e.getLocalizedMessage());
-		} finally {
-			cleanupObject(idlySelectionObj);
-		}
-	}
-
-	private void podiIdlySelectionTest(int i) throws Exception {
-
-		PodiIdlySelection podiIdlySelectionObj = new PodiIdlySelection();
-		try { 
-			windowHandle(driver);
-						qf.clickAction(driver, podiIdlySelectionObj._podiIdlyV, configFileObj.getBooleanProperty("podiIdlyV3"  + i + "click"));
-			if(configFileObj.getBooleanProperty("podiIdlyV3"  + i + "click"))
-			printSuccessLogAndReport( "Clicked on Podi Idly (V)");
-			windowHandle(driver);
-						windowHandle(driver);
-						qf.clickAction(driver, podiIdlySelectionObj._addtoCart, configFileObj.getBooleanProperty("addtoCart3"  + i + "click"));
-			if(configFileObj.getBooleanProperty("addtoCart3"  + i + "click"))
-			printSuccessLogAndReport( "Clicked on Add to Cart");
-			windowHandle(driver);
-						qf.clickAction(driver, podiIdlySelectionObj._checkout0, configFileObj.getBooleanProperty("checkout03"  + i + "click"));
-			if(configFileObj.getBooleanProperty("checkout03"  + i + "click"))
-			printSuccessLogAndReport( "Clicked on Checkout (0)");
-
-		} catch (Exception e) {
-			isElementDispalyed = false;
-			printFailureLogAndReport(   "Element is not found" + e.getLocalizedMessage());
-		} finally {
-			cleanupObject(podiIdlySelectionObj);
-		}
-	}
-
-	private void checkoutScreenTest(int i) throws Exception {
-
-		CheckoutScreen checkoutScreenObj = new CheckoutScreen();
-		try { 
-			windowHandle(driver);
-						qf.sendKeys(driver, checkoutScreenObj._yourfirstnamehere, configFileObj.getProperty("yourfirstnamehere4"  + i + "input"));
-			printSuccessLogAndReport( "Entered Your first name here input: " + configFileObj.getProperty("yourfirstnamehere4"  + i + "input"));
-			windowHandle(driver);
-						qf.sendKeys(driver, checkoutScreenObj._yourlastnamehere, configFileObj.getProperty("yourlastnamehere4"  + i + "input"));
-			printSuccessLogAndReport( "Entered Your last name here input: " + configFileObj.getProperty("yourlastnamehere4"  + i + "input"));
-			windowHandle(driver);
-						qf.sendKeys(driver, checkoutScreenObj._youremailhere, configFileObj.getProperty("youremailhere4"  + i + "input"));
-			printSuccessLogAndReport( "Entered Your email here input: " + configFileObj.getProperty("youremailhere4"  + i + "input"));
-			windowHandle(driver);
-						qf.sendKeys(driver, checkoutScreenObj._yourphonenumberhere, configFileObj.getProperty("yourphonenumberhere4"  + i + "input"));
-			printSuccessLogAndReport( "Entered Your phone number here input: " + configFileObj.getProperty("yourphonenumberhere4"  + i + "input"));
-			windowHandle(driver);
-						qf.clickAction(driver, checkoutScreenObj._cOD);
-			printSuccessLogAndReport( "Clicked on COD");
-			windowHandle(driver);
-						qf.clickAction(driver, checkoutScreenObj._cONTINUE, configFileObj.getBooleanProperty("cONTINUE4"  + i + "click"));
-			if(configFileObj.getBooleanProperty("cONTINUE4"  + i + "click"))
-			printSuccessLogAndReport( "Clicked on CONTINUE");
-
-		} catch (Exception e) {
-			isElementDispalyed = false;
-			printFailureLogAndReport(   "Element is not found" + e.getLocalizedMessage());
-		} finally {
-			cleanupObject(checkoutScreenObj);
-		}
-	}
-
 	private void orderConfirmedScreenTest(int i) throws Exception {
 
 		OrderConfirmedScreen orderConfirmedScreenObj = new OrderConfirmedScreen();
@@ -151,6 +66,91 @@ public class TC_OrderIdlyTestcase extends BaseClass {
 		}
 	}
 
+	private void idlySelectionTest(int i) throws Exception {
+
+		IdlySelection idlySelectionObj = new IdlySelection();
+		try { 
+			windowHandle(driver);
+						qf.wait(driver, idlySelectionObj._close);
+			qf.clickAction(driver, idlySelectionObj._close);
+			printSuccessLogAndReport( "Clicked on Close");
+			windowHandle(driver);
+						windowHandle(driver);
+						windowHandle(driver);
+						qf.clickAction(driver, idlySelectionObj._idlyV, configFileObj.getBooleanProperty("idlyV3"  + i + "click"));
+			if(configFileObj.getBooleanProperty("idlyV3"  + i + "click"))
+			printSuccessLogAndReport( "Clicked on Idly (V)");
+			windowHandle(driver);
+						qf.clickAction(driver, idlySelectionObj._addtoCart, configFileObj.getBooleanProperty("addtoCart3"  + i + "click"));
+			if(configFileObj.getBooleanProperty("addtoCart3"  + i + "click"))
+			printSuccessLogAndReport( "Clicked on Add to Cart");
+
+		} catch (Exception e) {
+			isElementDispalyed = false;
+			printFailureLogAndReport(   "Element is not found" + e.getLocalizedMessage());
+		} finally {
+			cleanupObject(idlySelectionObj);
+		}
+	}
+
+	private void podiIdlySelectionTest(int i) throws Exception {
+
+		PodiIdlySelection podiIdlySelectionObj = new PodiIdlySelection();
+		try { 
+			windowHandle(driver);
+						qf.clickAction(driver, podiIdlySelectionObj._podiIdlyV, configFileObj.getBooleanProperty("podiIdlyV4"  + i + "click"));
+			if(configFileObj.getBooleanProperty("podiIdlyV4"  + i + "click"))
+			printSuccessLogAndReport( "Clicked on Podi Idly (V)");
+			windowHandle(driver);
+						windowHandle(driver);
+						qf.clickAction(driver, podiIdlySelectionObj._addtoCart, configFileObj.getBooleanProperty("addtoCart4"  + i + "click"));
+			if(configFileObj.getBooleanProperty("addtoCart4"  + i + "click"))
+			printSuccessLogAndReport( "Clicked on Add to Cart");
+			windowHandle(driver);
+						qf.clickAction(driver, podiIdlySelectionObj._checkout0, configFileObj.getBooleanProperty("checkout04"  + i + "click"));
+			if(configFileObj.getBooleanProperty("checkout04"  + i + "click"))
+			printSuccessLogAndReport( "Clicked on Checkout (0)");
+
+		} catch (Exception e) {
+			isElementDispalyed = false;
+			printFailureLogAndReport(   "Element is not found" + e.getLocalizedMessage());
+		} finally {
+			cleanupObject(podiIdlySelectionObj);
+		}
+	}
+
+	private void checkoutScreenTest(int i) throws Exception {
+
+		CheckoutScreen checkoutScreenObj = new CheckoutScreen();
+		try { 
+			windowHandle(driver);
+						qf.sendKeys(driver, checkoutScreenObj._yourfirstnamehere, configFileObj.getProperty("yourfirstnamehere5"  + i + "input"));
+			printSuccessLogAndReport( "Entered Your first name here input: " + configFileObj.getProperty("yourfirstnamehere5"  + i + "input"));
+			windowHandle(driver);
+						qf.sendKeys(driver, checkoutScreenObj._yourlastnamehere, configFileObj.getProperty("yourlastnamehere5"  + i + "input"));
+			printSuccessLogAndReport( "Entered Your last name here input: " + configFileObj.getProperty("yourlastnamehere5"  + i + "input"));
+			windowHandle(driver);
+						qf.sendKeys(driver, checkoutScreenObj._youremailhere, configFileObj.getProperty("youremailhere5"  + i + "input"));
+			printSuccessLogAndReport( "Entered Your email here input: " + configFileObj.getProperty("youremailhere5"  + i + "input"));
+			windowHandle(driver);
+						qf.sendKeys(driver, checkoutScreenObj._yourphonenumberhere, configFileObj.getProperty("yourphonenumberhere5"  + i + "input"));
+			printSuccessLogAndReport( "Entered Your phone number here input: " + configFileObj.getProperty("yourphonenumberhere5"  + i + "input"));
+			windowHandle(driver);
+						qf.clickAction(driver, checkoutScreenObj._cOD);
+			printSuccessLogAndReport( "Clicked on COD");
+			windowHandle(driver);
+						qf.clickAction(driver, checkoutScreenObj._cONTINUE, configFileObj.getBooleanProperty("cONTINUE5"  + i + "click"));
+			if(configFileObj.getBooleanProperty("cONTINUE5"  + i + "click"))
+			printSuccessLogAndReport( "Clicked on CONTINUE");
+
+		} catch (Exception e) {
+			isElementDispalyed = false;
+			printFailureLogAndReport(   "Element is not found" + e.getLocalizedMessage());
+		} finally {
+			cleanupObject(checkoutScreenObj);
+		}
+	}
+
 	@Test
 	public void screensTest() throws Exception {
 		for(int datasets = 1; datasets <= configFileObj.getIntProperty("datasetsLength"); datasets++) {
@@ -158,10 +158,10 @@ public class TC_OrderIdlyTestcase extends BaseClass {
 			setup();			
 			setTestcaseName(browserName,"TC_OrderIdlyTestcase - " + configFileObj.getProperty("dataset" + (datasets)));
 			if(isElementDispalyed) {dashboardScreenTest(datasets);}
+			if(isElementDispalyed) {orderConfirmedScreenTest(datasets);}
 			if(isElementDispalyed) {idlySelectionTest(datasets);}
 			if(isElementDispalyed) {podiIdlySelectionTest(datasets);}
 			if(isElementDispalyed) {checkoutScreenTest(datasets);}
-			if(isElementDispalyed) {orderConfirmedScreenTest(datasets);}
 			tearDown();
 		}	}
 
